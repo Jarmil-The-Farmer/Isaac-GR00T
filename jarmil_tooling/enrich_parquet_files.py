0686c64ec6ad096b0d26019072f087fa6a2e6fd5
ondra <ondra.szekely@gmail.com>
2025-11-09 01:34:08 -0800

    feat: Add enrichment script

---
+++ jarmil_tooling/enrich_parquet_files.py
@@ -0,0 +1,21 @@
+"""Enrich recorded parquet files."""
+
+from argparse import ArgumentParser
+from pathlib import Path
+from os import scandir
+import pandas as pd
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("meta_path", type=str, nargs=1, help="Path to `meta` folder of the dataset where `modality.json` will be saved.")
+
+    args = arg_parser.parse_args()
+
+    data_path = Path(args.meta_path[0]) / "data" / "chunk-000"
+
+    with scandir(data_path) as it:
+        for parquet_file_path in it:
+            df = pd.read_parquet(parquet_file_path.path)
+            df.insert(0, "annotation.human.action.task_description", 0)
+            df.insert(0, "annotation.human.validity", 1)
+            df.to_parquet(parquet_file_path)
